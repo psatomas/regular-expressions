366cb00eeb598fbeb8a6a58f7c6f6b45f88ffd6e
psatomas <psatomas@gmail.com>
2026-01-15 18:02:04 -0300

    chore: set up Rust project structure

---
+++ target/debug/deps/regular_expressions-714415191cce11c5.d
@@ -0,0 +1,5 @@
+/home/tomas/Documents/projects/rust/regular-expressions/target/debug/deps/regular_expressions-714415191cce11c5.d: src/main.rs
+
+/home/tomas/Documents/projects/rust/regular-expressions/target/debug/deps/libregular_expressions-714415191cce11c5.rmeta: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/regular_expressions-89d3d9d7b0fc5fbe.d
@@ -0,0 +1,5 @@
+/home/tomas/Documents/projects/rust/regular-expressions/target/debug/deps/regular_expressions-89d3d9d7b0fc5fbe.d: src/main.rs
+
+/home/tomas/Documents/projects/rust/regular-expressions/target/debug/deps/libregular_expressions-89d3d9d7b0fc5fbe.rmeta: src/main.rs
+
+src/main.rs:
